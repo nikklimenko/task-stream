01df347646c17bca83c98fd25737800cf3a51ae7
Nik Klimenko <nikklimenko89@gmail.com>
2023-10-29 14:38:55 +0200

    feat: added navigation layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
   import {CheckCircleIcon} from "@heroicons/vue/24/solid";
+  import {ClockIcon, ListBulletIcon, ChartBarIcon} from "@heroicons/vue/24/outline";
 </script>
 
 <template>
@@ -8,7 +9,7 @@
       <img src="./assets/logo.png" alt="logo" class="h-9">
     </a>
     <a href="#">
-      <div v-if="true" class="flex items-center gap-1">
+      <div v-if="false" class="flex items-center gap-1">
         Day complete!
         <CheckCircleIcon class="h-7 text-green-500" />
       </div>
@@ -19,5 +20,26 @@
     </a>
   </header>
 
-  <main></main>
+  <main class="flex flex-grow flex-col">
+  </main>
+
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#timeline">
+          <ClockIcon class="h-6 w-6"/>timeline
+        </a>
+      </li>
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#activities">
+          <ListBulletIcon class="h-6 w-6"/>activities
+        </a>
+      </li>
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#progress">
+         <ChartBarIcon class="h-6 w-6"/>progress
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
