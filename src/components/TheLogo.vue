075d51ada8d70255a3b0036e176b6b37633726cf
Nik Klimenko <nikklimenko89@gmail.com>
2023-10-31 20:05:29 +0200

    refactor: made the header and nav components, highlighting the active item, hash normalization

---
+++ src/components/TheLogo.vue
@@ -0,0 +1,5 @@
+<template>
+  <a href="#" class="text-sm">
+    <img src="../assets/logo.png" alt="logo" class="h-9">
+  </a>
+</template>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {ClockIcon, ListBulletIcon, ChartBarIcon} from "@heroicons/vue/24/outline";
+import NavItem from "@/components/NavItem.vue";
+import {PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS} from '../constants';
+import {ref} from "vue";
+
+const navItems = {
+  [PAGE_TIMELINE] : ClockIcon,
+  [PAGE_ACTIVITIES] : ListBulletIcon,
+  [PAGE_PROGRESS] : ChartBarIcon
+};
+
+const currentPage = ref(normalizeHashPage());
+
+function normalizeHashPage() {
+  const hash = window.location.hash.slice(1);
+
+  if(Object.keys(navItems).includes(hash)){
+    return hash
+  }
+
+  window.location.hash = PAGE_TIMELINE;
+  return PAGE_TIMELINE;
+}
+
+</script>
+
+<template>
+  <nav class="sticky bottom-0 z-10 bg-white">
+
+    <ul class="flex items-center justify-around border-t">
+      <NavItem v-for="(icon, page) in navItems"
+               :key="page"
+               :href="`#${page}`"
+               :class="{ 'bg-gray-200 pointer-events-none' : page === currentPage }"
+               @click="currentPage = page">
+        <component :is="icon" class="h-6 w-6"/> {{ page }}
+      </NavItem>
+    </ul>
+  </nav>
+</template>
